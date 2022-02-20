5c27437c7a27e466da31f679a628154267c913c8
liuhubo <liuhubo@guigu.com>
2022-02-20 14:44:06 +0800

    first submit hot-fix

--- src/com/cy/linkedlist/GitTest.java
+++ src/com/cy/linkedlist/GitTest.java
@@ -3,5 +3,6 @@ package com.cy.linkedlist;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hellogit1");
+        System.out.println("hellogit2");
     }
 }
