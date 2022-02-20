c247e97c47eeabe3f4b1e220aa49c69a53d5098a
liuhubo <liuhubo@guigu.com>
2022-02-20 14:58:09 +0800

    third submit hot-fix

--- src/com/cy/linkedlist/GitTest.java
+++ src/com/cy/linkedlist/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hellogit1");
         System.out.println("hellogit2");
         System.out.println("hot-fix test");
+        System.out.println("hot-fix test2");
     }
 }
